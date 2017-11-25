ecc87dd7d55e5dca1715afcedb33e485146219b2
niksolaz <solazzo.nicola@gmail.com>
2017-11-25 14:39:23 +0100

    Insert login anonymous in firebase

--- testfirebase/testfirebase/ViewController.swift
+++ testfirebase/testfirebase/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loginAnonym()
     }
-
+    func loginAnonym() {
+        Auth.auth().signInAnonymously { (User, Error) in
+            if let error = Error {
+                print(error)
+            } else {
+                if let user = User {
+                    print(user.uid)
+                }
+            }
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
